f7752a442d889fba62092b2402d1df54dfd2ab70
francesco-scar <francesco_git@protonmail.com>
2021-06-22 11:11:03 +0200

    :zap: Change time per page

--- main/ESP32_main_sketch/LCD.ino
+++ main/ESP32_main_sketch/LCD.ino
@@ -1,7 +1,9 @@
 #ifndef DISABLE_LCD
 
 #define N_PAGES 29
-const int page_time_s[N_PAGES] = {15, 30, 45, 45, 45, 20, 50, 50, 15, 15, 30, 45, 45, 45, 20, 50, 50, 15, 15, 30, 45, 45, 45, 20, 50, 50, 15, 30, 60}
+//const int page_time_s[N_PAGES] = {10, 60, 40, 45, 60, 70, 80, 30, 60, 30, 15, 15, 20, 70, 70, 30, 10,  5, 30, 15, 30, 45, 60, 25, 60, 70, 10, 45, 120}
+const int page_time_s[N_PAGES] = { 5, 45, 30, 35, 45, 65, 70, 25, 50, 25, 10, 10, 10, 60, 60, 20,  5,  1, 25,  5, 25, 35, 55, 20, 55, 60, 115, 40, 30};
+//                                 1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26   27  28  29
 #include <LiquidCrystal_Software_I2C.h>     // https://github.com/francesco-scar/LiquidCrystal_Software_I2C
 
 #define SDA_PIN 26
